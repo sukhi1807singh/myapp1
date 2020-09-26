a4ed1d79991a0fd9f3846736b6506093b6270038
sukhjinder <sukhi1807singh@gmail.com>
2020-09-27 00:05:50 +0530

    new design

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('welcome to the homepage buddy');
+	res.send('Welcome to New Deployment');
 });
 
 const port = process.env.port || 3000;
